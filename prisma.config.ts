5baea06a6ce1bcfde00f4d6c46bc6ce90e292687
jcol4 <jpcollins2@dons.usfca.edu>
2026-03-31 12:56:44 -0700

    fixed issue with prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
   datasource: {
     url: process.env.DATABASE_URL!,
+    directUrl: process.env.DIRECT_URL!,
   },
 })
