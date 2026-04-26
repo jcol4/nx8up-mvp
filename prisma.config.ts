55e13231efc08124a64a51657aa1bbc9071cba44
jcol04 <jpcollins2@dons.usfca.edu>
2026-04-25 17:15:26 -0700

    verxcel fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,5 @@ export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
   datasource: {
     url: process.env.DATABASE_URL!,
-    directUrl: process.env.DIRECT_URL!,
   },
 })
